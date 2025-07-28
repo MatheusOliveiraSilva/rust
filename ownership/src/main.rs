77875dc4af49b225fd2699bea8f14506d10363bd
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-07-28 07:32:43 -0300

    slices impact at memory safety

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,11 +1,18 @@
 fn main() {
-    let mut s = String::from("hello");
+    let s = String::from("hello world");
 
-    change(&mut s);
+    let word = first_word(&s);
 
-    println!("{}", s);
+    println!("{}", word);
 }
 
-fn change(s: &mut String) {
-    s.push_str(", world");
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
 }

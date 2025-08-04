3187851b21e27fd825e0f8d938b886b237a2ef48
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-08-04 09:47:51 -0300

    removing comments

--- exercises/src/employees.rs
+++ exercises/src/employees.rs
@@ -63,22 +63,17 @@ enum Command {
 }
 
 pub fn cli_interface() {
-
-    // Using a hash map and vectors, create a text interface to allow a user to add employee names 
-    // to a department in a company; for example, “Add Sally to Engineering” or “Add Amir to Sales.” 
-    // Then let the user retrieve a list of all people in a department or all people in the company by department, 
-    // sorted alphabetically.
     
     let mut system = EmployeeSystem {
         employess_by_department: HashMap::new()
     };
 
+    println!("Welcome to employees management system.");
+    println!("Use expression 'Add <employee> to <departament>' to add a employee to a department.");
+    println!("Use /show_all to show all people in the company by department.");
+    println!("Use /<department> to show all people in a department.");
+
     loop {
-        
-        println!("Welcome to employees management system.");
-        println!("Use expression 'Add <employee> to <departament>' to add a employee to a department.");
-        println!("Use /show_all to show all people in the company by department.");
-        println!("Use /<department> to show all people in a department.");
 
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).expect("Failed to read line");

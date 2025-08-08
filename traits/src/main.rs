b757bad521dd1eae58a99a824a2b567a9a258435
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-08-08 20:57:50 -0300

    learning lifetimes and traits

---
+++ traits/src/main.rs
@@ -0,0 +1,16 @@
+mod summary;
+
+use summary::{SocialPost, Summary};
+
+fn main() {
+    let post = SocialPost {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        repost: false,
+    };
+
+    println!("1 new post: {}", post.summarize());
+}
